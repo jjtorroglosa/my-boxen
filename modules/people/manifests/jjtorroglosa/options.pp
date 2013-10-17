b60b60b0adee1efbcf8de70b121bd7fe4a6c7140
Juanjo Torroglosa <jjtorroglosa@gmail.com>
2013-10-17 21:22:10 +0200

    Added full keyboard support

--- modules/people/manifests/jjtorroglosa/options.pp
+++ modules/people/manifests/jjtorroglosa/options.pp
@@ -3,4 +3,5 @@ class people::jjtorroglosa::options {
   include osx::finder::show_all_on_desktop
   include osx::dock::autohide
   include osx::global::expand_save_dialog
+  include osx::global::enable_keyboard_control_access
 }
